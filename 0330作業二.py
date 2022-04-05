e78094472a92dabaae0c8db69e5cb41cf4e228b1
munin1010 <102035562+munin1010@users.noreply.github.com>
2022-04-05 19:59:02 +0800

    Add files via upload

---
+++ 0330作業二.py
@@ -0,0 +1,9 @@
+# 
+"""
+輸入3個值,a,b,c
+呼叫函式,由大至小順序輸出
+"""
+
+from homework20330 import sor
+sor()
+
---
+++ homework20330.py
@@ -0,0 +1,15 @@
+# 作業二模組
+def sor():
+    data=[]
+    while len(data)<3:
+        x=int(input("請輸入"))
+        data.append(x)
+        data.sort()
+    print(data)
+
+    
+
+                    
+        
+       
+    
